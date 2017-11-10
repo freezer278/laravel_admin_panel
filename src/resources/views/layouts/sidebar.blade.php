d5f79ad9af27bf721df2a4499aa4662d8862cc8a
Vladimir Morozov <freezer278@gmail.com>
2017-11-10 14:46:11 +0200

    fixed displaying current user name

--- src/resources/views/layouts/sidebar.blade.php
+++ src/resources/views/layouts/sidebar.blade.php
@@ -3,25 +3,28 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
         <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="https://placehold.it/160x160/00a65a/ffffff/&text=u" class="img-circle" alt="User Image">
+
+        @if (Auth::check())
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="https://placehold.it/160x160/00a65a/ffffff/&text=u" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                    <p>{{ Auth::user()->name }}</p>
+                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                </div>
             </div>
-            <div class="pull-left info">
-                <p>{{ Auth::user()->name or '' }}</p>
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
+        @endif
+        {{--<!-- search form -->--}}
+        {{--<form action="#" method="get" class="sidebar-form">--}}
+            {{--<div class="input-group">--}}
+                {{--<input type="text" name="q" class="form-control" placeholder="Search...">--}}
+                {{--<span class="input-group-btn">--}}
+                {{--<button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>--}}
+                {{--</button>--}}
+              {{--</span>--}}
+            {{--</div>--}}
+        {{--</form>--}}
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
