a9c95dbcd624dc88b51349219ff11e10aa8539b7
Vladimir Morozov <freezer278@gmail.com>
2017-12-07 14:35:32 +0200

    improved service provider to be able to publish and load all lang files

--- src/AdminGeneratorServiceProvider.php
+++ src/AdminGeneratorServiceProvider.php
@@ -18,12 +18,15 @@ class AdminGeneratorServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(resource_path('views/vendor/vmorozov/laravel_admin_generator'), self::VIEWS_NAME);
 
+        $this->loadTranslationsFrom('langs/vendor/vmorozov/laravel_admin_generator', self::VIEWS_NAME);
+
 //        $this->loadViewsFrom(realpath(__DIR__.'../resources/views'), self::VIEWS_NAME);
 
 
 //        $this->publishFiles();
 
         $this->publishes([__DIR__.'/resources/views' => resource_path('views/vendor/vmorozov/laravel_admin_generator')], 'views');
+        $this->publishes([__DIR__.'/resources/lang' => resource_path('lang/vendor/vmorozov/laravel_admin_generator')], 'lang');
 
         $this->publishes([base_path('vendor/almasaeed2010/adminlte/dist') => public_path('adminlte')], 'adminlte');
         $this->publishes([base_path('vendor/almasaeed2010/adminlte/bower_components') => public_path('adminlte/bower_components')], 'adminlte');
