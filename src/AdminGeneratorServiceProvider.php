ff69b3fa2b98b4979fe4e9e7c82ed68f8d5e9e21
Vladimir Morozov <freezer278@gmail.com>
2018-03-03 18:33:51 +0200

    another publish code fix

--- src/AdminGeneratorServiceProvider.php
+++ src/AdminGeneratorServiceProvider.php
@@ -36,7 +36,7 @@ class AdminGeneratorServiceProvider extends ServiceProvider
         $this->publishes([base_path('vendor/almasaeed2010/adminlte/dist') => public_path('adminlte')], 'adminlte');
         $this->publishes([base_path('vendor/almasaeed2010/adminlte/bower_components') => public_path('adminlte/bower_components')], 'adminlte');
         $this->publishes([base_path('vendor/almasaeed2010/adminlte/plugins') => public_path('adminlte/plugins')], 'adminlte');
-        $this->publishes([base_path(__DIR__.'/resources/assets/js') => public_path('laravel_admin_generator/js')], 'js');
+        $this->publishes([__DIR__.'/resources/assets/js' => public_path('laravel_admin_generator/js')], 'js');
 
         if ($this->app->runningInConsole()) {
             $this->commands([]);
