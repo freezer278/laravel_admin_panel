1a43c78ecd8806e53a7149929f22bc051a558096
Vitaly-PHP-dev <v.korako@gmail.com>
2018-09-27 16:12:27 +0300

    Add CKEditor plugin, config, deploy

---
+++ src/resources/views/forms/field_types/ckeditor.blade.php
@@ -0,0 +1,21 @@
+<div class="form-group col-md-12 {{ $errors->has($fieldName) ? 'has-error' : '' }}">
+    <label for="{{ $fieldName }}">{{ $field->getLabel() }}</label>
+
+    <textarea name="{{ $fieldName }}"
+              id="{{ $fieldName }}"
+              class="form-control"
+              {{ $field->required() ? 'required' : '' }}
+              minlength="{{ isset($params[\Vmorozov\LaravelAdminGenerator\App\Utils\Field::PARAM_KEY_MIN]) ? $params[\Vmorozov\LaravelAdminGenerator\App\Utils\Field::PARAM_KEY_MIN] : '' }}"
+              maxlength="{{ isset($params[\Vmorozov\LaravelAdminGenerator\App\Utils\Field::PARAM_KEY_MAX]) ? $params[\Vmorozov\LaravelAdminGenerator\App\Utils\Field::PARAM_KEY_MAX] : '' }}"
+    >{{ $entity->$fieldName or old($fieldName) }}</textarea>
+
+    @if ($errors->has($fieldName))
+        <span class="help-block">
+            <strong>{{ $errors->first($fieldName) }}</strong>
+        </span>
+    @endif
+
+    <script>
+        CKEDITOR.replace( '{{ $fieldName }}' );
+    </script>
+</div>
